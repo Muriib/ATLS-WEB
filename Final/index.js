dcec507f9ca8b0508659481897c0939ce99bc64a
Muriib <brmu5032@colorado.edu>
2021-12-11 22:01:26 -0700

    final

---
+++ Final/index.js
@@ -0,0 +1,277 @@
+console.log("hello");
+
+document.getElementById("mmpr").addEventListener("mouseover", oneon)
+document.getElementById("mmpr").addEventListener("mouseout", oneout)
+document.getElementById("zeo").addEventListener("mouseover", twoon)
+document.getElementById("zeo").addEventListener("mouseout", twoout)
+document.getElementById("turbo").addEventListener("mouseover", threeon)
+document.getElementById("turbo").addEventListener("mouseout", threeout)
+document.getElementById("space").addEventListener("mouseover", fouron)
+document.getElementById("space").addEventListener("mouseout", fourout)
+document.getElementById("galaxy").addEventListener("mouseover", fiveon)
+document.getElementById("galaxy").addEventListener("mouseout", fiveout)
+document.getElementById("lr").addEventListener("mouseover", sixon)
+document.getElementById("lr").addEventListener("mouseout", sixout)
+document.getElementById("tf").addEventListener("mouseover", sevenon)
+document.getElementById("tf").addEventListener("mouseout", sevenout)
+document.getElementById("wf").addEventListener("mouseover", eighton)
+document.getElementById("wf").addEventListener("mouseout", eightout)
+document.getElementById("ns").addEventListener("mouseover", nineon)
+document.getElementById("ns").addEventListener("mouseout", nineout)
+document.getElementById("dt").addEventListener("mouseover", tenon)
+document.getElementById("dt").addEventListener("mouseout", tenout)
+document.getElementById("spd").addEventListener("mouseover", elevenon)
+document.getElementById("spd").addEventListener("mouseout", elevenout)
+document.getElementById("mf").addEventListener("mouseover", twelveon)
+document.getElementById("mf").addEventListener("mouseout", twelveout)
+document.getElementById("oo").addEventListener("mouseover", thirteenon)
+document.getElementById("oo").addEventListener("mouseout", thirteenout)
+document.getElementById("jf").addEventListener("mouseover", fourteenon)
+document.getElementById("jf").addEventListener("mouseout", fourteenout)
+document.getElementById("rpm").addEventListener("mouseover", fifteenon)
+document.getElementById("rpm").addEventListener("mouseout", fifteenout)
+document.getElementById("sam").addEventListener("mouseover", sixteenon)
+document.getElementById("sam").addEventListener("mouseout", sixteenout)
+document.getElementById("ssam").addEventListener("mouseover", seventeenon)
+document.getElementById("ssam").addEventListener("mouseout", seventeenout)
+document.getElementById("mega").addEventListener("mouseover", eighteenon)
+document.getElementById("mega").addEventListener("mouseout", eighteenout)
+document.getElementById("smega").addEventListener("mouseover", nineteenon)
+document.getElementById("smega").addEventListener("mouseout", nineteenout)
+document.getElementById("dc").addEventListener("mouseover", twentyon)
+document.getElementById("dc").addEventListener("mouseout", twentyout)
+document.getElementById("sdc").addEventListener("mouseover", twentyoneon)
+document.getElementById("sdc").addEventListener("mouseout", twentyoneout)
+document.getElementById("nst").addEventListener("mouseover", twentytwoon)
+document.getElementById("nst").addEventListener("mouseout", twentytwoout)
+document.getElementById("snst").addEventListener("mouseover", twentythreeon)
+document.getElementById("snst").addEventListener("mouseout", twentythreeout)
+document.getElementById("bm").addEventListener("mouseover", twentyfouron)
+document.getElementById("bm").addEventListener("mouseout", twentyfourout)
+document.getElementById("dn").addEventListener("mouseover", twentyfiveon)
+document.getElementById("dn").addEventListener("mouseout", twentyfiveout)
+
+function oneon(){
+  document.getElementById("mmpr").style.opacity = "0.2";
+  document.getElementById("txt1").style.display = "initial";
+}
+function oneout(){
+  document.getElementById("mmpr").style.opacity = "1";
+  document.getElementById("txt1").style.display = "none";
+}
+
+function twoon(){
+  document.getElementById("zeo").style.opacity = "0.2";
+  document.getElementById("txt2").style.display = "initial";
+}
+function twoout(){
+  document.getElementById("zeo").style.opacity = "1";
+  document.getElementById("txt2").style.display = "none";
+}
+
+function threeon(){
+  document.getElementById("turbo").style.opacity = "0.2";
+  document.getElementById("txt3").style.display = "initial";
+}
+function threeout(){
+  document.getElementById("turbo").style.opacity = "1";
+  document.getElementById("txt3").style.display = "none";
+}
+
+function fouron(){
+  document.getElementById("space").style.opacity = "0.2";
+  document.getElementById("txt4").style.display = "initial";
+}
+function fourout(){
+  document.getElementById("space").style.opacity = "1";
+  document.getElementById("txt4").style.display = "none";
+}
+
+function fiveon(){
+  document.getElementById("galaxy").style.opacity = "0.2";
+  document.getElementById("txt5").style.display = "initial";
+}
+function fiveout(){
+  document.getElementById("galaxy").style.opacity = "1";
+  document.getElementById("txt5").style.display = "none";
+}
+
+function sixon(){
+  document.getElementById("lr").style.opacity = "0.2";
+  document.getElementById("txt6").style.display = "initial";
+}
+function sixout(){
+  document.getElementById("lr").style.opacity = "1";
+  document.getElementById("txt6").style.display = "none";
+}
+
+function sevenon(){
+  document.getElementById("tf").style.opacity = "0.2";
+  document.getElementById("txt7").style.display = "initial";
+}
+function sevenout(){
+  document.getElementById("tf").style.opacity = "1";
+  document.getElementById("txt7").style.display = "none";
+}
+
+function eighton(){
+  document.getElementById("wf").style.opacity = "0.2";
+  document.getElementById("txt8").style.display = "initial";
+}
+function eightout(){
+  document.getElementById("wf").style.opacity = "1";
+  document.getElementById("txt8").style.display = "none";
+}
+
+function nineon(){
+  document.getElementById("ns").style.opacity = "0.2";
+  document.getElementById("txt9").style.display = "initial";
+}
+function nineout(){
+  document.getElementById("ns").style.opacity = "1";
+  document.getElementById("txt9").style.display = "none";
+}
+
+function tenon(){
+  document.getElementById("dt").style.opacity = "0.2";
+  document.getElementById("txt10").style.display = "initial";
+}
+function tenout(){
+  document.getElementById("dt").style.opacity = "1";
+  document.getElementById("txt10").style.display = "none";
+}
+
+function elevenon(){
+  document.getElementById("spd").style.opacity = "0.2";
+  document.getElementById("txt11").style.display = "initial";
+}
+function elevenout(){
+  document.getElementById("spd").style.opacity = "1";
+  document.getElementById("txt11").style.display = "none";
+}
+
+function twelveon(){
+  document.getElementById("mf").style.opacity = "0.2";
+  document.getElementById("txt12").style.display = "initial";
+}
+function twelveout(){
+  document.getElementById("mf").style.opacity = "1";
+  document.getElementById("txt12").style.display = "none";
+}
+
+function thirteenon(){
+  document.getElementById("oo").style.opacity = "0.2";
+  document.getElementById("txt13").style.display = "initial";
+}
+function thirteenout(){
+  document.getElementById("oo").style.opacity = "1";
+  document.getElementById("txt13").style.display = "none";
+}
+
+function fourteenon(){
+  document.getElementById("jf").style.opacity = "0.2";
+  document.getElementById("txt14").style.display = "initial";
+}
+function fourteenout(){
+  document.getElementById("jf").style.opacity = "1";
+  document.getElementById("txt14").style.display = "none";
+}
+
+function fifteenon(){
+  document.getElementById("rpm").style.opacity = "0.2";
+  document.getElementById("txt15").style.display = "initial";
+}
+function fifteenout(){
+  document.getElementById("rpm").style.opacity = "1";
+  document.getElementById("txt15").style.display = "none";
+}
+
+function sixteenon(){
+  document.getElementById("sam").style.opacity = "0.2";
+  document.getElementById("txt16").style.display = "initial";
+}
+function sixteenout(){
+  document.getElementById("sam").style.opacity = "1";
+  document.getElementById("txt16").style.display = "none";
+}
+
+function seventeenon(){
+  document.getElementById("ssam").style.opacity = "0.2";
+  document.getElementById("txt17").style.display = "initial";
+}
+function seventeenout(){
+  document.getElementById("ssam").style.opacity = "1";
+  document.getElementById("txt17").style.display = "none";
+}
+
+function eighteenon(){
+  document.getElementById("mega").style.opacity = "0.2";
+  document.getElementById("txt18").style.display = "initial";
+}
+function eighteenout(){
+  document.getElementById("mega").style.opacity = "1";
+  document.getElementById("txt18").style.display = "none";
+}
+
+function nineteenon(){
+  document.getElementById("smega").style.opacity = "0.2";
+  document.getElementById("txt19").style.display = "initial";
+}
+function nineteenout(){
+  document.getElementById("smega").style.opacity = "1";
+  document.getElementById("txt19").style.display = "none";
+}
+
+function twentyon(){
+  document.getElementById("dc").style.opacity = "0.2";
+  document.getElementById("txt20").style.display = "initial";
+}
+function twentyout(){
+  document.getElementById("dc").style.opacity = "1";
+  document.getElementById("txt20").style.display = "none";
+}
+
+function twentyoneon(){
+  document.getElementById("sdc").style.opacity = "0.2";
+  document.getElementById("txt21").style.display = "initial";
+}
+function twentyoneout(){
+  document.getElementById("sdc").style.opacity = "1";
+  document.getElementById("txt21").style.display = "none";
+}
+
+function twentytwoon(){
+  document.getElementById("nst").style.opacity = "0.2";
+  document.getElementById("txt22").style.display = "initial";
+}
+function twentytwoout(){
+  document.getElementById("nst").style.opacity = "1";
+  document.getElementById("txt22").style.display = "none";
+}
+
+function twentythreeon(){
+  document.getElementById("snst").style.opacity = "0.2";
+  document.getElementById("txt23").style.display = "initial";
+}
+function twentythreeout(){
+  document.getElementById("snst").style.opacity = "1";
+  document.getElementById("txt23").style.display = "none";
+}
+
+function twentyfouron(){
+  document.getElementById("bm").style.opacity = "0.2";
+  document.getElementById("txt24").style.display = "initial";
+}
+function twentyfourout(){
+  document.getElementById("bm").style.opacity = "1";
+  document.getElementById("txt24").style.display = "none";
+}
+
+function twentyfiveon(){
+  document.getElementById("dn").style.opacity = "0.2";
+  document.getElementById("txt25").style.display = "initial";
+}
+function twentyfiveout(){
+  document.getElementById("dn").style.opacity = "1";
+  document.getElementById("txt25").style.display = "none";
+}
